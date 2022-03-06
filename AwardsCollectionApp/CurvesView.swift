a86bb8d4f41bfebd621aa47e3596215d7049c9bb
Elisei Mishchenko <esm.mail@icloud.com>
2022-03-06 20:31:23 +0300

    Creating a CurvesView file in which there will be an image from curves

---
+++ AwardsCollectionApp/CurvesView.swift
@@ -0,0 +1,20 @@
+//
+//  CurvesView.swift
+//  AwardsCollectionApp
+//
+//  Created by Елисей Мищенко on 06.03.2022.
+//
+
+import SwiftUI
+
+struct CurvesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CurvesView_Previews: PreviewProvider {
+    static var previews: some View {
+        CurvesView()
+    }
+}
