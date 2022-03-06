769e2f4168fb43b2f23024f4f7c49f888fdf736d
Elisei Mishchenko <esm.mail@icloud.com>
2022-03-06 20:22:41 +0300

    Creating squares and adding an airplane

--- AwardsCollectionApp/GradientRectangles.swift
+++ AwardsCollectionApp/GradientRectangles.swift
@@ -9,12 +9,39 @@ import SwiftUI
 
 struct GradientRectangles: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            let height = geometry.size.height
+        
+            ZStack {
+                ForEach(0..<3) { iteration in
+                    Rectangle()
+                        .fill(
+                            LinearGradient(
+                                gradient: Gradient(colors: [.green, .blue]),
+                                startPoint: UnitPoint(x: 0, y: 1),
+                                endPoint: UnitPoint(x: 1, y: 0)
+                            )
+                    )
+                        .rotationEffect(.degrees(Double(iteration) * 60))
+                        .frame(
+                            width: width * 0.7,
+                            height: height * 0.7
+                        )
+                }
+                Image(systemName: "airplane")
+                    .resizable()
+                    .rotationEffect(.degrees(-90))
+                    .opacity(0.5)
+                    .scaleEffect(0.7)
+            }
+        }
     }
 }
 
 struct GradientRectangles_Previews: PreviewProvider {
     static var previews: some View {
         GradientRectangles()
+            .frame(width: 200, height: 200)
     }
 }
