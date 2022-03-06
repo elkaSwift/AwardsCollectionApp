aede6c977e57e0403d38dffe8ed731263b4437f1
Elisei Mishchenko <esm.mail@icloud.com>
2022-03-06 20:03:05 +0300

    Renaming a file from contentView to StarterView

--- AwardsCollectionApp/StarterView.swift
+++ AwardsCollectionApp/StarterView.swift
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct StarterView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct StarterView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        StarterView()
     }
 }
