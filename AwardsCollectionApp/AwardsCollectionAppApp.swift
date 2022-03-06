872128c589b4629a295a80a71c310d86be460f6a
Elisei Mishchenko <esm.mail@icloud.com>
2022-03-06 19:59:46 +0300

    Initial Commit

---
+++ AwardsCollectionApp/AwardsCollectionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardsCollectionAppApp.swift
+//  AwardsCollectionApp
+//
+//  Created by Елисей Мищенко on 06.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct AwardsCollectionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AwardsCollectionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AwardsCollectionApp
+//
+//  Created by Елисей Мищенко on 06.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
