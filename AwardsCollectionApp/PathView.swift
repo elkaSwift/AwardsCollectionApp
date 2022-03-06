1e61a6162185f24460667a425961e01e66a2f015
Elisei Mishchenko <esm.mail@icloud.com>
2022-03-06 20:28:12 +0300

    Creating a PathView file in which there will be a road with a car

---
+++ AwardsCollectionApp/PathView.swift
@@ -0,0 +1,20 @@
+//
+//  PathView.swift
+//  AwardsCollectionApp
+//
+//  Created by Елисей Мищенко on 06.03.2022.
+//
+
+import SwiftUI
+
+struct PathView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PathView_Previews: PreviewProvider {
+    static var previews: some View {
+        PathView()
+    }
+}
