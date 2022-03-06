c136ee71c334b90fe067ac5c9672331a4b206039
Elisei Mishchenko <esm.mail@icloud.com>
2022-03-06 20:24:41 +0300

    Creating a GradientCircle file in which a circle with a car will be created

---
+++ AwardsCollectionApp/GradientCircle.swift
@@ -0,0 +1,20 @@
+//
+//  GradientCircle.swift
+//  AwardsCollectionApp
+//
+//  Created by Елисей Мищенко on 06.03.2022.
+//
+
+import SwiftUI
+
+struct GradientCircle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GradientCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientCircle()
+    }
+}
