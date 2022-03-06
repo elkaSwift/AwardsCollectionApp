5b70ed65c48e095135e6b3f8c3042312328d4a37
Elisei Mishchenko <esm.mail@icloud.com>
2022-03-06 20:20:43 +0300

    Creating a GradientRestangles file in which squares with an airplane will be created

---
+++ AwardsCollectionApp/GradientRectangles.swift
@@ -0,0 +1,20 @@
+//
+//  GradientRectangles.swift
+//  AwardsCollectionApp
+//
+//  Created by Елисей Мищенко on 06.03.2022.
+//
+
+import SwiftUI
+
+struct GradientRectangles: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GradientRectangles_Previews: PreviewProvider {
+    static var previews: some View {
+        GradientRectangles()
+    }
+}
