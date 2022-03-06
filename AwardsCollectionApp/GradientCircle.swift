8abb7835bb31d571f44722e6f9304e361cb548c6
Elisei Mishchenko <esm.mail@icloud.com>
2022-03-06 20:26:39 +0300

    Creating a gradient circle with a car inside

--- AwardsCollectionApp/GradientCircle.swift
+++ AwardsCollectionApp/GradientCircle.swift
@@ -9,12 +9,38 @@ import SwiftUI
 
 struct GradientCircle: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            let width = geometry.size.width
+            let height = geometry.size.height
+            
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [.yellow, .pink]),
+                            startPoint: UnitPoint(x: 1, y: 0),
+                            endPoint: UnitPoint(x: 0, y: 1)
+                        )
+                    )
+                    .frame(
+                        width: width * 0.7,
+                        height: height * 0.7
+                    )
+                    .shadow(radius: 15)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                
+                Image(systemName: "car")
+                    .resizable()
+                    .scaleEffect(0.3)
+                    .colorInvert()
+            }
+        }
     }
 }
 
 struct GradientCircle_Previews: PreviewProvider {
     static var previews: some View {
         GradientCircle()
+            .frame(width: 300, height: 300)
     }
 }
